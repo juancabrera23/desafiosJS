559c82c5461dd5d1639f05015e8218e7da3d9859
juan g cabrera <cabrerajuang18@gmail.com>
2021-09-01 15:14:29 -0300

    modificado

--- clase1.js
+++ clase1.js
@@ -1,3 +1,3 @@
 var nombre= prompt("ingrese su nombre");
 var mascota= prompt("ingrese el nombre de su mascota");
-alert("bienvenodo a cachorritus " + nombre + ", ya puedes empezar a comprar para " + mascota)
+alert("bienvenido a cachorritus " + nombre + ", ya puedes empezar a comprar para " + mascota)
