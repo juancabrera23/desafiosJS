84cc8687c0e1063599a10913fe3a32b318c40cfb
juan gabriel cabrera <cabrerajuang18@gmail.com>
2021-09-23 22:16:02 -0300

    agregando Array

--- clase5.js
+++ clase5.js
@@ -7,3 +7,6 @@ var producto1 = new Producto("Royal Canin", 1800, "perro");
 console.log(producto1);
 var producto2 = new Producto("Royal Cat", 580, "gato");
 console.log(producto2);
+
+var compra = [producto1, producto2];
+console.log(compra)
